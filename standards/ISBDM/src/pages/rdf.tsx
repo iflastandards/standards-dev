04440f1a0a5749c66f1359eed819acce9583a621
Jon Phipps <jphipps@madcreek.com>
2025-06-17 13:43:18 -0400

    feat: introduce RDF Downloads placeholder pages for all standards
    - Added placeholder pages for RDF Downloads under ISBDM, MulDiCat, ISBD, LRM, UNIMARC, and FRBR
    - Included consistent layout, titles, and "Coming soon" messages for future updates
    - Streamlined design to prepare for RDF data availability across standards

---
+++ standards/ISBDM/src/pages/rdf.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import Layout from '@theme/Layout';
+
+export default function RDFDownloads(): React.ReactElement {
+  return (
+    <Layout
+      title="RDF Downloads"
+      description="Download RDF data for ISBD for Manifestation">
+      <main className="container margin-vert--lg">
+        <div className="row">
+          <div className="col col--8 col--offset-2">
+            <h1>RDF Downloads</h1>
+            <p className="hero__subtitle">
+              Coming soon... RDF downloads will be available from this page
+            </p>
+          </div>
+        </div>
+      </main>
+    </Layout>
+  );
+}
